118f3ab9af7251e58247cd2ea005d2b546442a4a
longhoang <slimmy.hoang@gmail.com>
2022-03-05 13:28:48 +0700

    bai tap

---
+++ if-else/tank/cal.js
@@ -0,0 +1,12 @@
+function moveUp() {
+    document.getElementById("tank").style["top"] = `${15}px`
+}
+function moveDown() {
+    document.getElementById("tank").style["top"] = `${30}px`
+}
+function moveRight() {
+    document.getElementById("tank").style["left"] = `${38}px`
+}
+function moveLeft() {
+    document.getElementById("tank").style["left"] = `${20}px`
+}
